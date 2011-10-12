4e30c43c59085d9a7a6bdaeab6193c5542639033
Constantinos Xanthopoulos <conx@akyroi.gr>
2011-10-12 22:08:19 +0300

    Function names, Per year chart & template beautification

--- MailmanStats.py
+++ MailmanStats.py
@@ -17,8 +17,9 @@ class Authors:
         self.totalthreads = 0
         self.totalmonth = {}
         self.prevmsgtime = 0
+        self.years = []
 
-    def parse_msg(self, msg):
+    def parseMsg(self, msg):
         if (msg.from_mail not in self.authors):
             author = Author(msg.from_mail, msg.date)
             self.authors[msg.from_mail] = author
@@ -41,21 +42,24 @@ class Authors:
             self.totalmonth[msg.month] += 1
         self.prevmsgtime = msg.date
 
-    def parse_dates(self):
+    def calcStats(self):
+        self.sortAuthors()
+        self.createUserPages()
+        self.plotEmailsPerAuthor()
+        self.plotThreadsPerAuthor()
+        self.plotMonthlyUsage()
+
+    def parseDates(self):
         for a in self.authors:  
             pass    # Throws ValueError: timestamp out of range for platform time_t Probably needs another converter
-            #self.authors[a].shorttime = time.strftime('%d %b %Y %H:%M:%S', time.gmtime(self.authors[a].shorttime))
+        #self.authors[a].shorttime = time.strftime('%d %b %Y %H:%M:%S', time.gmtime(self.authors[a].shorttime))
             #self.authors[a].longtime = time.strftime('%d %b %Y %H:%M:%S', time.gmtime(self.authors[a].longtime)) 
 
-    def print_authors(self):
-        for author in self.authors:
-            print(self.authors[author])
-
-    def sort_authors(self):
+    def sortAuthors(self):
         self.sorted_authors_emails = sorted(self.authors, key=lambda x:self.authors[x].posts, reverse=True)
         self.sorted_authors_threads = sorted(self.authors, key=lambda x:self.authors[x].started, reverse=True)
 
-    def create_pages(self):
+    def createUserPages(self):
         for a in self.authors:
             f = open(outputdir+"/ml-files/"+self.authors[a].pagename, 'w')
             t = pyratemp.Template(filename='user.tpl')
@@ -68,11 +72,45 @@ class Authors:
             tmp.append([a, authors.authors[a].posts])
         plotBarGraph(tmp, outputdir+"/ml-files/ml-emailsperauthor.png", "Authors", "Emails")
 
-    def plotThreadsPerAuthor(self): #FIXME sorting
+    def plotThreadsPerAuthor(self):
         tmp = []
         for a in self.sorted_authors_threads:
-                tmp.append([a, authors.authors[a].started])
+            tmp.append([a, authors.authors[a].started])
         plotBarGraph(tmp, outputdir+"/ml-files/ml-threadsperauthor.png", "Authors", "Threads")
+
+    def plotMonthlyUsage(self):
+        months = ["January", "February", "March", "April", "May", "June", "Julu", "August", "September", "Octomber", "November", "December"]
+        firstyear = int(min(self.totalmonth.keys())[:4])
+        lastyear = int(max(self.totalmonth.keys())[:4])
+        firstmonth = int(min(self.totalmonth.keys())[-2:])
+        peryear = []
+        r = ""
+        y = firstyear
+        m = firstmonth
+        while r != max(self.totalmonth.keys()):
+            m += 1
+            if m % 12 == 1:
+                m = 1
+                y += 1
+            r = "%i%02d" % (y,m)
+            if r not in self.totalmonth:
+                self.totalmonth[r] = 0
+
+        for year in range(firstyear, lastyear+1):
+            self.years.append(year)
+            for yearmonth in self.totalmonth.keys():
+                if int(yearmonth[:4]) == year:
+                    peryear.append([months[int(yearmonth[-2:])-1], self.totalmonth[yearmonth]])
+            peryear = sorted(peryear, key=lambda x: months.index(x[0]))
+            plotBarGraph(peryear, outputdir+"/ml-files/ml-usage-"+str(year)+".png", "Emails", "Months")
+            peryear = []
+
+
+    def __str__(self):
+        for author in self.authors:
+            print(self.authors[author])
+
+
 
 # Represents the author of the post probably a subscriber of the list
 class Author:
@@ -83,40 +121,41 @@ class Author:
         self.lastmsgdate = date
         self.lastmsgdatestr = time.ctime(date)
         self.firstmsgdate = time.ctime(date) 
-        self.name = self.get_name(self.mail)
-        self.pagename = self.get_pagename(self.mail)
+        self.name = self.getName(self.mail)
+        self.pagename = self.getPagename(self.mail)
         self.monthdic = {}
-        self.shorttime = 999999999999999999999999999999999999
+        self.shorttime = sys.maxint
         self.longtime = 0
 
-    def get_pagename(self, mail):
+    def getPagename(self, mail):
         mail = mail.replace('@', 'at')
         return  "ml-" + mail + ".html"
 
-    def get_name(self, mail):
+    def getName(self, mail):
         at = mail.find('@')
         return mail[:at]
- 
+
     def __str__(self):
         return self.mail+" "+str(self.posts)+" "+str(self.started)+" "+self.lastmsgdate
 
 class Message:
     def __init__(self, message):
         self.subject = message['subject']
-        self.from_mail = self.get_mail(message['from'])
+        self.from_mail = self.getMail(message['from'])
         r = re.match("[^,]*[,][ ]+([0-9]+[ ]+[A-Za-z]{3}[ ]+[0-9]{4}[ ]+[0-9:]{8}).*", message['date'])
         if r:
             t = time.strptime(r.group(1), '%d %b %Y %H:%M:%S')
             self.date = time.mktime(t)
-            self.month = self.get_month(r.group(1))
+            self.month = self.getMonth(r.group(1))
 
-    def get_month(self, date):
-        x1 = date.find(' ') + 1
-        x2 = date.replace(' ', '_', 2).find(' ')
-        return date[x1:x2].replace(' ', '_')
-        
+    def getMonth(self, date):
+        r = re.match("[0-9]+[ ]([A-Za-z]{3})[ ]([0-9]{4}).*", date)
+        months = {"Jan": 1, "Feb": 2, "Mar": 3, "Apr": 4, "May": 5, "Jun": 6, "Jul": 7, "Aug": 8, "Sep": 9, "Oct": 10, "Nov": 11, "Dec": 12}
+        month = dictSub(r.group(1), months)
+        return "%s%02d" % (r.group(2), int(month))
+
     # Returns the content between the signs [<, >] 
-    def get_mail(self, string):
+    def getMail(self, string):
         x1 = string.find('<') + 1
         x2 = string.find('>')
         return string[x1:x2]
@@ -135,15 +174,22 @@ def plotBarGraph(data, outputfile, xlabel, ylabel):
     ar.add_plot(bar_plot.T(data = data, fill_style = fs))
     ar.draw()
 
-def get_mlname(mboxpath):
+def getMlName(mboxpath):
     dot = path.basename(mboxpath).find(".")
     return path.basename(args[0])[:dot]
+
+
+def dictSub(text, dictionary):
+    prog = re.compile('|'.join(map(re.escape, dictionary)))
+    return prog.sub(str(dictionary[prog.match(text).group(0)]), text)
+
 
 if __name__ == "__main__":
     parser = OptionParser(usage="usage: %prog [options] <mbox file>")
     parser.add_option("-g", "--graph", default=False, dest="graph", action="store_true", help="Add graphs to the report")
     parser.add_option("-o", "--output", default="./", dest="output", help="Use this option to change the output directory. Default: Current working directory.")
     (options, args) = parser.parse_args()
+
     # Arguments validation
     if len(args) < 1:
         parser.print_help()
@@ -157,7 +203,7 @@ if __name__ == "__main__":
     outputfile = "ml-report.html"
     outputdir = options.output
     authors = Authors()
-    mlname = get_mlname(args[0])
+    mlname = getMlName(args[0])
 
     # Create Directory for extra files
     try:
@@ -165,25 +211,18 @@ if __name__ == "__main__":
     except OSError:
         pass
 
-
     # Parse all messages in mbox file
     for message in mbox:
         msg = Message(message)
-        authors.parse_msg(msg)
+        authors.parseMsg(msg)
 
-    authors.parse_dates() 
-    authors.create_pages() 
-    authors.sort_authors()
+    authors.calcStats()
 
-    #authors.print_authors() #FIXME Debug info
-    
+    #  Generate ml-report.html
     f = open(outputfile, 'w')
     t = pyratemp.Template(filename='report.tpl')
-    result = t(heading=mlname, totalmails=authors.totalmails, totalthreads=authors.totalthreads, mydic=authors.authors, sa=authors.sorted_authors_emails)
+    result = t(heading=mlname, totalmails=authors.totalmails, totalthreads=authors.totalthreads, mydic=authors.authors, sa=authors.sorted_authors_emails, yr=authors.years)
     f.write(result)
     f.close()
-
-    authors.plotEmailsPerAuthor()
-    authors.plotThreadsPerAuthor()
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
--- report.tpl
+++ report.tpl
@@ -14,20 +14,33 @@ th
 {
      text-align: left;
 }
+h3
+{
+	background-color: lightblue;
+}
 </style>
 </head>
 <body>
-<h1>@!heading!@ Mailing List Stats</h1>
+<h1>'@!heading!@' Mailing List Stats</h1>
+<h3>General Statistics</h3>
 <table>
 <tr><th>Total Mails:</th><td>@!totalmails!@</td></tr>
 <tr><th>Total Threads:</th><td>@!totalthreads!@</td></tr>
 </table>
+<h3>Charts</h3>
 <div style="padding: 1em; width: 520px;">
 <div style="width: 250px; float: left;"><a href="ml-files/ml-emailsperauthor.png"><img src="ml-files/ml-emailsperauthor.png" alt="Emails Per Author" height=150 width=250></br><center>Emails Per Author</center></a></div>
 <div style="width: 250px; float: left;"><a href="ml-files/ml-threadsperauthor.png"><img src="ml-files/ml-threadsperauthor.png" alt="Emails Per Author" height=150 width=250></br><center>Threads Per Author</center></a></div>
 <div style="clear: both"></div>
 </div>
 </br>
+<h3>Yearly Usage Charts</h3>
+<ul>
+<!--(for i in yr)-->
+<li><a href="ml-files/ml-usage-@!i!@.png">@!i!@</a></li>
+<!--(end)-->
+</ul>
+<h3>User Statistics</h3>
 <table class="sortable">
 <tr><th>Name</th><th>Mails Sent</th><th>Threads Started</th><th>Last Message</th></tr>
 <!--(for i in sa)-->
